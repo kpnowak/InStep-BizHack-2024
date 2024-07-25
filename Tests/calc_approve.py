db848c101011ec035c1b4d040c0d0ee4f3e7d0ff
kpnowak <krzysztof.nowak.krakow@gmail.com>
2024-07-25 07:11:30 +0530

    Changes in approval calculation

--- Tests/calc_approve.py
+++ Tests/calc_approve.py
@@ -13,12 +13,20 @@ if data['forecasted_discount'] > 20:
     if response_place1.status_code == 200 and response_place1.headers.get('Content-Type') == 'application/json':
         response_json = response_place1.json()
         
+        # Save response JSON to a file for the Node.js server to read
+        with open('proposal_data.json', 'w') as outfile:
+            json.dump(response_json, outfile)
+        
         # Send the response JSON to Place 2
         response_place2 = requests.post('http://localhost:3000/place2', json=response_json)
         print('Response from Place 2:', response_place2.text)
     else:
         print('Invalid response from Place 1')
 else:
+    # Save the data directly to a file for the Node.js server to read
+    with open('proposal_data.json', 'w') as outfile:
+        json.dump(data, outfile)
+    
     # Send to Place 2 directly
     response_place2 = requests.post('http://localhost:3000/place2', json=data)
     print('Response from Place 2:', response_place2.text)
